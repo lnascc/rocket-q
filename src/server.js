93db107eed332f93a837e408d6d3b275e7579a10
Larissa Nascimento <larissa.nascc15@gmail.com>
2022-01-20 20:16:10 -0300

    comeco backend

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ server.use(express.static('public')) //carrega arquivos estáticos como imgs, cs
 
 server.set('views', path.join(__dirname, 'views'))
 
+server.use(express.urlencoded({ extended: true }))
+
 server.use(route)
 
 server.listen(3000, () => console.log('rodando'))
